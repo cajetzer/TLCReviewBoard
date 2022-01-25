fd6041cefc85a9993ada8ead06065d485c69b8f0
cajetzer <64230564+cajetzer@users.noreply.github.com>
2022-01-25 15:59:07 -0500

    added unpack script

--- SolutionManagement.ps1
+++ SolutionManagement.ps1
@@ -1,10 +1,42 @@
-# Set these before running the script, I put the solution files in the solution folder
+# AUTH SNIPPETS
+pac auth create --kind DATAVERSE --url https://hlstechreviewdev.crm.dynamics.com/ --name TLCRevewBoardDev
+pac auth create --kind DATAVERSE --url https://hlstechreview.crm.dynamics.com/ --name TLCRevewBoardProd
 
-$solutionDir = "C:\Users\cajetzer\OneDrive - Microsoft\Documents\GitHub\CCSRB-Project\Solutions\"
-$solutionVersion = "1.0.0.0"
+pac auth list
+pac auth who
+pac auth select --index 3
+
+# EXPORT SOLUTIONS
+# Set these before running the script
+
+$solutionDir = ".\Solutions"
+$solutionVersion = "1.0.0.1"
+$newSolutionVersion = "1.0.0.2"
 $solutionName = "TechReview"
 
 # The following commands execute the export using the variables above
 
 pac solution export --path ${solutionDir}${solutionName}-${solutionVersion}.zip --name $solutionName --managed false
 pac solution export --path ${solutionDir}${solutionName}-${solutionVersion}_managed.zip --name $solutionName --managed true
+pac solution online-version --solution-name ${solutionName} --solution-version ${newSolutionVersion}
+
+# IMPORT MANAGED SOLUTION
+# Set these before running the script
+
+$solutionDir = "C.\Solutions\"
+$solutionVersion = "1.0.0.1"
+$solutionName = "TechReview"
+
+# The following commands execute the export using the variables above
+
+pac solution import --path ${solutionDir}${solutionName}-${solutionVersion}_managed.zip
+
+# UNPACK SOLUTIONS
+# Set these before running script
+
+$solutionDir = ".\Solutions\"
+$solutionVersion = "1.0.0.1"
+$solutionName = "TechReview"
+$targetFolder = ".\Source\"
+
+pac solution unpack --zipfile ${solutionDir}${solutionName}-${solutionVersion}.zip --folder ${targetFolder}
