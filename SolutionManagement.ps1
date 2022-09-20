bf6035b55959eec22ae5609b4b75a5e36fc5d895
cajetzer <64230564+cajetzer@users.noreply.github.com>
2022-09-20 17:25:50 -0400

    added settings file commands for using environment variables

--- SolutionManagement.ps1
+++ SolutionManagement.ps1
@@ -4,14 +4,19 @@ pac auth create --kind DATAVERSE --url https://hlstechreview.crm.dynamics.com/ -
 
 pac auth list
 pac auth who
-pac auth select --index 3
+pac auth select --index 2
+
+# Environment variable values for saving
+# MOCK channel ID: 19:9b3390f9b3ff4eb0821164774eeaae35@thread.tacv2
+# general channel ID: 19:VZvuSWaqpI8YfBoCJ9wg5dMDhdIHaybmn6BgS7dzaZ41@thread.tacv2
+
 
 # EXPORT SOLUTIONS
 # Set these before running the script
 
 $solutionDir = ".\Solutions\"
-$solutionVersion = "1.1.0.9"
-$newSolutionVersion = "1.1.0.10"
+$solutionVersion = "1.1.0.10"
+$newSolutionVersion = "1.1.0.11"
 $solutionName = "TechReview"
 
 # The following commands execute the export using the variables above
@@ -20,22 +25,28 @@ pac solution export --path ${solutionDir}${solutionName}-${solutionVersion}.zip 
 pac solution export --path ${solutionDir}${solutionName}-${solutionVersion}_managed.zip --name $solutionName --managed true
 pac solution online-version --solution-name ${solutionName} --solution-version ${newSolutionVersion}
 
+# Generate Settings File for environment variables
+pac solution create-settings -z ${solutionDir}${solutionName}-${solutionVersion}.zip -s ${solutionDir}EnvSettings.json
+
 # IMPORT MANAGED SOLUTION
 # Set these before running the script
 
-$solutionDir = "C.\Solutions\"
-$solutionVersion = "1.2.0.9"
+$solutionDir = ".\Solutions\"
+$solutionVersion = "1.1.0.10"
 $solutionName = "TechReview"
+$settingsName = "EnvSettings.json"
 
 # The following commands execute the export using the variables above
 
-pac solution import --path ${solutionDir}${solutionName}-${solutionVersion}_managed.zip
+# pac solution import --path ${solutionDir}${solutionName}-${solutionVersion}_managed.zip
+# OR import with settings file
+pac solution import --path ${solutionDir}${solutionName}-${solutionVersion}_managed.zip --settings-file ${solutionDir}${settingsName}
 
 # UNPACK SOLUTIONS
 # Set these before running script
 
 $solutionDir = ".\Solutions\"
-$solutionVersion = "1.0.0.1"
+$solutionVersion = "1.1.0.10"
 $solutionName = "TechReview"
 $targetFolder = ".\Source\"
 
